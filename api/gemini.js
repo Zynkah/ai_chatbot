99797784305d85bac21f4cc7ad558593727527e8
Zynkah <ffgurl07@yahoo.com>
2025-05-01 17:54:58 -0500

    testing api

--- api/gemini.js
+++ api/gemini.js
@@ -1,19 +1,19 @@
-// export default async function handler(req, res) {
-//   const { prompt } = req.body;
-//   const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+export default async function handler(req, res) {
+  const { prompt } = req.body;
+  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 
-//   const response = await fetch(
-//     "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=" +
-//       apiKey,
-//     {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         contents: [{ parts: [{ text: prompt }] }],
-//       }),
-//     }
-//   );
+  const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=" +
+      apiKey,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        contents: [{ parts: [{ text: prompt }] }],
+      }),
+    }
+  );
 
-//   const data = await response.json();
-//   res.status(200).json(data);
-// }
+  const data = await response.json();
+  res.status(200).json(data);
+}
