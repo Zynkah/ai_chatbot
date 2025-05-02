d862f60c184d7ac697cb446b60793e78d78bd297
Zynkah <ffgurl07@yahoo.com>
2025-05-02 09:41:06 -0500

    typos fixed

--- src/widgets/MoreInfo.jsx
+++ src/widgets/MoreInfo.jsx
@@ -5,8 +5,8 @@ export const MoreInfo = () => {
     <div className="answerWrapper">
       <p>
         For more information on Fareplay or Privy click this link to visit our
-        officail
-        <a src="/"> Frequently Asked Questions page.</a>
+        official
+        <a href="/" target="_blank" rel="noopener noreferrer"> Frequently Asked Questions page.</a>
       </p>
     </div>
   );
