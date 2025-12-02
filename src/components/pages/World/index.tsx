c554c995a58edf886d34a47993f67b3048a88fea
Zynkah <ffgurl07@yahoo.com>
2025-12-02 15:15:47 -0600

    mapped

--- src/components/pages/World/index.tsx
+++ src/components/pages/World/index.tsx
@@ -1,27 +1,16 @@
 import { GridLayout } from "../../GridLayout";
 import { icons } from "../../Icons";
+import WorldMapDataJson from "../../../data/WorldMapData.json";
 
 export const World = () => {
+  const { WorldMapData } = WorldMapDataJson;
   return (
     <GridLayout
       title_1="World Map Locations"
       content_block_1={
         <>
-          
-          {/* todo map this mofo */}
-          {icons("bunker", "../Bunker.webp", "bunker image", 150)}
-          {icons(
-            "abandoned factory",
-            "../Abandoned_Factory.webp",
-            "abandoned factory",
-            150
-          )}
-          {icons("city ruins", "../City_Ruins.webp", "city ruins", 150)}
-           {icons(
-            "resistance camp",
-            "../Resistance_Camp.webp",
-            "resistance camp",
-            165
+          {WorldMapData.map((location) =>
+            icons(location.title, location.image, location.alt, location.width)
           )}
         </>
       }
